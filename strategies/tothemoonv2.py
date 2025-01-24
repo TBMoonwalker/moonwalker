804add78a2d891b755bf9d07846cfe52aeb99b9c
TBMoonwalker <>
2025-01-24 07:18:57 +0100

    fix timeframe for strategy

--- strategies/tothemoonv2.py
+++ strategies/tothemoonv2.py
@@ -4,9 +4,10 @@ from filter import Filter
 
 
 class Strategy:
-    def __init__(self, ws_url, loglevel, btc_pulse, currency):
+    def __init__(self, ws_url, loglevel, btc_pulse, currency, timeframe):
         self.ws_url = ws_url
         self.btc_pulse = btc_pulse
+        self.timeframe = timeframe
 
         Strategy.logging = LoggerFactory.get_logger(
             "logs/strategies.log", "tothemoon", log_level=loglevel
@@ -23,11 +24,11 @@ class Strategy:
                 btc_pulse = self.filter.btc_pulse_status("5Min", "10Min")
 
             if btc_pulse:
-                ema_slope_50 = self.filter.ema_slope(symbol, "15Min", 50).json()
-                ema_slope_9 = self.filter.ema_slope(symbol, "15Min", 9).json()
-                rsi_slope_14 = self.filter.rsi_slope(symbol, "15Min", 14).json()
-                ema_cross_15m = self.filter.ema_cross(symbol, "15Min").json()
-                # rsi = self.filter.get_rsi(symbol, "15Min").json()
+                ema_slope_50 = self.filter.ema_slope(symbol, self.timeframe, 50).json()
+                ema_slope_9 = self.filter.ema_slope(symbol, self.timeframe, 9).json()
+                rsi_slope_14 = self.filter.rsi_slope(symbol, self.timeframe, 14).json()
+                ema_cross = self.filter.ema_cross(symbol, self.timeframe).json()
+                # rsi = self.filter.get_rsi(symbol, self.timeframe).json()
                 # rsi_value = float(rsi["status"])
                 # support_level_30m = self.filter.support_level(symbol, "1d", 10).json()
                 # support_level = support_level_30m["status"]
@@ -36,7 +37,7 @@ class Strategy:
                 Strategy.logging.debug(f"EMA slope 9: {ema_slope_9}")
                 Strategy.logging.debug(f"EMA slope 50: {ema_slope_50}")
                 Strategy.logging.debug(f"RSI slope 14: {rsi_slope_14}")
-                Strategy.logging.debug(f"EMA cross: {ema_cross_15m}")
+                Strategy.logging.debug(f"EMA cross: {ema_cross}")
                 # TODO: Implement extreme wick detection (high percentage down in one candle)
                 # Strategy.logging.debug(f"Support Level: {support_level}")
                 # Strategy.logging.debug(f"RSI value: {rsi_value}")
@@ -46,7 +47,7 @@ class Strategy:
                     ema_slope_9["status"] == "upward"
                     and ema_slope_50["status"] == "upward"
                     and rsi_slope_14["status"] == "upward"
-                ) and ema_cross_15m["status"] == "up":
+                ) and ema_cross["status"] == "up":
                     # create SO
                     result = True
             else:
