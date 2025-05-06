eb06edc71839c48d8835a8f1e338fd0f6c37c7e7
TBMoonwalker <>
2025-05-06 17:45:37 +0200

    add bbands_cross indicator and strategy

---
+++ strategies/bbands_cross.py
@@ -0,0 +1,36 @@
+import helper
+from service.filter import Filter
+from service.indicators import Indicators
+
+logging = helper.LoggerFactory.get_logger("logs/strategies.log", "bbands")
+
+
+class Strategy:
+    def __init__(self, timeframe, btc_pulse=None):
+        self.timeframe = timeframe
+        self.filter = Filter()
+        self.indicators = Indicators()
+
+    async def run(self, symbol):
+        result = False
+
+        try:
+            bbands = await self.indicators.calculate_bbands_cross(
+                symbol, self.timeframe, 50
+            )
+
+            if bbands:
+                # create SO
+                result = True
+
+            logging_json = {
+                "symbol": symbol,
+                "bbands": bbands,
+                "creating_order": result,
+            }
+            logging.debug(f"{logging_json}")
+
+        except ValueError as e:
+            logging.error(f"JSON Message is garbage: {e}")
+
+        return result
