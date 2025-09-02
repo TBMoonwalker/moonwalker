6ea93e537312514f2fe7e9c33fc28320893735ac
TBMoonwalker <>
2025-09-02 08:25:54 +0200

    add catch for missing indicator data

--- strategies/ema_low.py
+++ strategies/ema_low.py
@@ -22,24 +22,27 @@ class Strategy:
             symbol, self.timeframe, [20, 50, 100, 200]
         )
         close = await self.indicators.get_close_price(symbol, self.timeframe, 5)
+        try:
+            if (
+                ema["ema_20"] < ema["ema_200"]
+                and ema["ema_50"] < ema["ema_200"]
+                and ema["ema_100"] < ema["ema_200"]
+            ):
+                # Check if rebound happened
+                if close.dropna().iloc[-2] > ema["ema_20"]:
+                    logging.debug(
+                        f"Price rebound from EMA down for {symbol} Close: {close.dropna().iloc[-2]} Previous close: {close.dropna().iloc[-3]}"
+                    )
+                    result = True
 
-        if (
-            ema["ema_20"] < ema["ema_200"]
-            and ema["ema_50"] < ema["ema_200"]
-            and ema["ema_100"] < ema["ema_200"]
-        ):
-            # Check if rebound happened
-            if close.dropna().iloc[-2] > ema["ema_20"]:
-                logging.debug(
-                    f"Price rebound from EMA down for {symbol} Close: {close.dropna().iloc[-2]} Previous close: {close.dropna().iloc[-3]}"
-                )
-                result = True
-
-        logging_json = {
-            "symbol": symbol,
-            "ema(20/50/100/200)": f"{ema["ema_20"]}, {ema["ema_50"]}, {ema["ema_100"]}, {ema["ema_200"]}",
-            "close price(last)": f"{close.dropna().iloc[-2]}",
-            "creating_order": result,
-        }
-        logging.debug(f"{logging_json}")
+            logging_json = {
+                "symbol": symbol,
+                "ema(20/50/100/200)": f"{ema["ema_20"]}, {ema["ema_50"]}, {ema["ema_100"]}, {ema["ema_200"]}",
+                "close price(last)": f"{close.dropna().iloc[-2]}",
+                "creating_order": result,
+            }
+            logging.debug(f"{logging_json}")
+        except:
+            logging.debug("Cannot run strategy, check indicators.log")
+            return False
         return result
